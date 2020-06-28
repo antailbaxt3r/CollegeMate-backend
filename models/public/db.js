6ce9bbcb318c713495f263c2bfbbf43a41708f8d
OmSingh <omsingh6903@gmail.com>
2020-06-28 17:51:28 +0530

    updated build

--- models/public/db.js
+++ models/public/db.js
@@ -17,10 +17,10 @@ const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DA
       require: true,
       rejectUnauthorized: false
     }
-  },
+  }, 
   operatorAliases: false,
   // socketPath : env.SOCKET_PATH,
-  //dialectOptions: env.DIALECT_OPTIONS
+  dialectOptions: env.DIALECT_OPTIONS
 //  logging: false
 });
 

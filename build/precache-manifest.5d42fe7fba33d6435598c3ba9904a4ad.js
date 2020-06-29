633e9c29ec835f6f7538607e9f6af7147d261276
OmSingh <omsingh6903@gmail.com>
2020-06-30 03:42:55 +0530

    updated build

---
+++ build/precache-manifest.5d42fe7fba33d6435598c3ba9904a4ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed04ae6a582e74dc2f97211accafa303",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0386574ba58d046b0354",
+    "url": "./static/css/main.190f1e24.chunk.css"
+  },
+  {
+    "revision": "f9c1a1be8ade50382d2d",
+    "url": "./static/js/2.ef6d926a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.ef6d926a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0386574ba58d046b0354",
+    "url": "./static/js/main.9e6841cc.chunk.js"
+  },
+  {
+    "revision": "015a752f298bc4d1b44a",
+    "url": "./static/js/runtime-main.dd3e920c.js"
+  },
+  {
+    "revision": "46818ebd4f76c4e6fe9b030dbc74f5cf",
+    "url": "./static/media/Raleway-Black.46818ebd.ttf"
+  },
+  {
+    "revision": "bb5ae98e4ce1a64042093dc235c305ed",
+    "url": "./static/media/Raleway-Medium.bb5ae98e.ttf"
+  }
+]);
